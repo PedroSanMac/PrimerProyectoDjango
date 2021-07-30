1afa69156305fc02eb0b04ddddb9cc9e54efa494
Pedr0 <psanchezm@unsa.edu.pe>
2021-07-30 00:35:48 -0500

    Se creo templates y otra vista con contexto, se prepara para trabajar con forms

--- personas/views.py
+++ personas/views.py
@@ -1,3 +1,11 @@
+from personas.models import Persona
 from django.shortcuts import render
 
 # Create your views here.
+def personaTestView(request):
+    obj = Persona.objects.get(id = 1)
+    context = {
+        'nombre' : obj.nombres,
+        'edad' : obj.edad,
+    }
+    return render(request, 'personas/test.html', context)
